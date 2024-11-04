8f0825483f708e21589d8fe978520244a59905e0
maxbogucki <bogucki.max@gmail.com>
2024-11-04 09:58:39 -0600

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,3 @@
-// Max Bogucki
-// CS318
-// P01
-// Walk to a certain amount of points a certain amount of times and get different data based on results
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -41,45 +36,54 @@ int main() {
 		cout << endl;
 	}
 
-	cout << "Walks Sorted: " << endl;
+	sort(walks.begin(), walks.end(), [](const Walk& a, const Walk& b) {
+        	return a.getDistance() < b.getDistance();
+   	});
 
-	vector<double> distances;
-
-	// Finds distances and displays data
-	double totalDistance = 0, shortest = 0, median = 0, longest = 0;
-	for (int i = 0; i < walks.size(); i++) {
-		double currDistance = walks[i].getDistance();
-		distances.push_back(currDistance);
-
-		cout << "	Point: " << walks[i].getX() << ", " << walks[i].getY() << " Distance: " << walks[i].getDistance() << endl;
-		totalDistance += walks[i].getDistance();
-
-		// Finds shortest distance
-		if (currDistance < shortest || i == 0) {
-			shortest = currDistance;
-		}
-
-		// Finds median distance
-		sort(distances.begin(), distances.end());
-		int size = distances.size();
-		if (size % 2 == 0) {
-			median = (distances[size / 2 - 1] + distances[size / 2]) / 2;
-		}
-		else {
-			median = distances[size / 2];
-		}
-
-		// Finds longest distance
-		if (currDistance > longest) {
-			longest = currDistance;
-		}
+	cout << "Walks Sorted by Distance: " << endl;
+	for(const auto& walk : walks) {
+		cout << "Point: " << walk.getX() << ", " << walk.getY()
+		     << " Distance: " << walk.getDistance() << endl;
 	}
-	cout << endl;
 	
 	// Display averages and different distances
-	cout << "Average Distance: " << totalDistance / numWalks << endl << endl;
+	cout << "Average Distance: " << calculateAverageDistance(walks) << endl;
+	cout << "Shortest Distance/Walk: " << findShortestDistance(walks) << endl;
+	cout << "Median Distance/Walk: " << findMedianDistance(walks) << endl;
+	cout << "Longest Distance/Walk: " << findLongestDistance(walks) << endl;
 
-	cout << "Shortest Distance/Walk: " << shortest << endl;
-	cout << "Median Distance/Walk: " << median << endl;
-	cout << "Longest Distance/Walk: " << longest << endl;
+	return 0;
+}
+
+double calculateAverageDistance(const vector<Walk>& walks) {
+    double totalDistance = 0;
+    for (const auto& walk : walks) {
+        totalDistance += walk.getDistance();
+    }
+    return totalDistance / walks.size();
+}
+
+double findShortestDistance(const vector<Walk>& walks) {
+    double shortest = numeric_limits<double>::max();
+    for (const auto& walk : walks) {
+        if (walk.getDistance() < shortest) {
+            shortest = walk.getDistance();
+        }
+    }
+    return shortest;
+}
+
+double findMedianDistance(vector<double>& distances) {
+    size_t size = distances.size();
+    return (size % 2 == 0) ? (distances[size / 2 - 1] + distances[size / 2]) / 2.0 : distances[size / 2];
+}
+
+double findLongestDistance(const vector<Walk>& walks) {
+    double longest = 0;
+    for (const auto& walk : walks) {
+        if (walk.getDistance() > longest) {
+            longest = walk.getDistance();
+        }
+    }
+    return longest;
 }
